488fe4c1cb8220090e021b6cef824e81b288c54f
William Garcia <114087502+EarthWarrior621@users.noreply.github.com>
2023-03-23 09:33:09 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace conditionals___william_garcia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks user for whole number and assigns to integer variable
+            Console.WriteLine("Enter a whole number, please!");
+            int myWholeNum = Convert.ToInt32(Console.ReadLine());
+
+            // Checks if postitve or negtive
+            if (myWholeNum >= 0)
+            {
+                Console.WriteLine($"{myWholeNum} is a positive number!");
+            }
+            else
+            {
+                Console.WriteLine($"{myWholeNum} is a negative number!");  
+            }
+
+            // Checksif even or odd
+            if(myWholeNum % 2 == 0)
+            {
+                Console.WriteLine($"{myWholeNum} is an even number!");
+            }
+            else
+            {
+                Console.WriteLine($"{myWholeNum} is an even number!");
+            }
+
+            // Creates two more integer numbers
+            int num1 = 6;
+            int num2 = 99;
+           
+            // Gets first max
+            int firstMax = Math.Max(myWholeNum, num1);
+            // Gets maximum number
+            int max = Math.Max(firstMax, num2);
+            
+            // Prints max to console
+            Console.WriteLine($"The maximum number of {myWholeNum}, {num1}, and {num2} is {max}!");
+
+            // Asks user to enter a greade and assigns it to a char variable, grade
+            Console.WriteLine("Enter a grade (A, B, C, D, F)!");
+            char grade = Console.ReadKey().KeyChar;
+
+            // Prints description based on grade value
+            switch (grade)
+            {
+                case 'a':
+                    Console.WriteLine("\nSuperior");
+                    break;
+                case 'B':
+                    Console.WriteLine("\nVery Good");
+                    break;
+                case 'C':
+                    Console.WriteLine("\nAverage");
+                    break;
+                case 'D':
+                    Console.WriteLine("\nBelow Average");
+                    break;
+                case 'F':
+                    Console.WriteLine("\nFail");
+                    break;
+                default:
+                    Console.WriteLine("\nYou did not enter a valid grade!");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# conditionals---william-garcia
+ 
